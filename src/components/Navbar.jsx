6999b00099e40ee1006f136a9ec191c024188198
Dumindu420 <167874993+Dumindu420@users.noreply.github.com>
2025-11-01 01:53:33 +0530

    Refactor context imports to use 'useAppContext' hook and update Navbar search functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import vmsLogo from '../assets/VMS logo.png'
 import profileIcon from '../assets/man.png'
+import { useAppContext } from '../context/useAppContext'
 
 const Navbar = () => {
     const [open, setOpen] = React.useState(false)
@@ -9,6 +10,7 @@ const Navbar = () => {
     const [user, setUser] = React.useState(null)
     const location = useLocation()
     const hideIcons = location && location.pathname === '/login'
+    const { searchQuery, setSearchQuery } = useAppContext()
     
     const handleLogoClick = () => {
         window.location.href = '/'
@@ -40,7 +42,7 @@ const Navbar = () => {
 
                             {!hideIcons && (
                                 <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
-                                    <input className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
+                                    <input className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
                                     <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M10.836 10.615 15 14.695" stroke="#7A7B7D" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
                                         <path clipRule="evenodd" d="M9.141 11.738c2.729-1.136 4.001-4.224 2.841-6.898S7.67.921 4.942 2.057C2.211 3.193.94 6.281 2.1 8.955s4.312 3.92 7.041 2.783" stroke="#7A7B7D" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
